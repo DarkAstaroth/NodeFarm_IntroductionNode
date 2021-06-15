2f9441d17e3ea01d819e6713bac0177a9a17666f
Manuel <mninahuanca4@gmail.com>
2021-06-15 15:29:04 -0400

    Building a very simple API

--- index.js
+++ index.js
@@ -33,21 +33,28 @@ const url = require('url');
 
 ////////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-   
+
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
         res.end('This is the overview!');
     } else if (pathName === '/product') {
         res.end('This is the product!');
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
-            'my-own-header':'hello-world'
+            'my-own-header': 'hello-world'
         });
         res.end('<h1>Page not found</h1>');
     }
-    /// res.end('Hello from the server!');
 });
 
 server.listen(8000, '127.0.0.1', () => {
